5af799a04b290186432d85fe61763c1afb0cd5fb
Maria Vitoria <91562068+vivihzg@users.noreply.github.com>
2025-09-10 19:56:48 -0300

    entrega trabalho

--- src/main/java/com/lucasmartins/mineiracaodados/controller/TurmaController.java
+++ src/main/java/com/lucasmartins/mineiracaodados/controller/TurmaController.java
@@ -1,14 +1,56 @@
 package com.lucasmartins.mineiracaodados.controller;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+
 @RestController
+@RequestMapping("/turmas")
+
 public class TurmaController {
 
-    @GetMapping("/turma")
-    public String buscarTurma() {
-        return "T04N - Mineração de dados";
+    @GetMapping("/nome")
+    @ResponseStatus(HttpStatus.OK)
+    public String buscarNomeTurma() {
+        return "T03N Mineração de Dados";
     }
 
+    @GetMapping("/quantidade")
+    public int getQuantidadeAlunos() {
+        return 30;
+    }
+
+    @PostMapping
+    public String criarTurma() {
+        return "Turma T03N criada";
+    }
+
+    @PutMapping
+    public String atualizarTurma() {
+        return "Turma T03N atualizada";
+    }
+
+    @DeleteMapping
+    public void deletarTurma() {
+
+    }
+
+    @GetMapping("/disciplina")
+    public String getDisciplina() {
+        return "Mineração de Dados";
+    }
+
+    @GetMapping("/alunos")
+    public List<String> getListaAlunos() {
+        return Arrays.asList("Maria", "Thiago", "Larissa", "Crysthian", "Gislaine");
+    }
 }
